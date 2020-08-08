f9853e71bb27b0fd267350d1e116cb094578b60c
Gerasim Gerasimov <dialix@yandex.ru>
2020-08-08 16:06:58 +0700

    добавил полезные ссылки по теме React Canvas

--- src/pages/Trands/TViewBox.tsx
+++ src/pages/Trands/TViewBox.tsx
@@ -1,3 +1,7 @@
+//https://philna.sh/blog/2018/09/27/techniques-for-animating-on-the-canvas-in-react/
+//https://codepen.io/vasilly/pen/NRKyWL
+//https://stackoverflow.com/questions/53661473/canvas-animation-reactjs-with-requestanimationframe
+
 import React, {Component} from 'react'
 import './Trands.css'
 import { TTrandHeight } from '../../lib/trands/trandsgroup'
@@ -11,7 +15,7 @@ export interface IViewBoxProps {
 export default class TViewBox extends Component<IViewBoxProps, {}> {
     private height: TTrandHeight;
     private viewBox: TViewBoxModel;
-    private canasRef: any = React.createRef();
+    private canvasRef: any = React.createRef();
 
     constructor (props: any){
       super(props);
@@ -20,7 +24,8 @@ export default class TViewBox extends Component<IViewBoxProps, {}> {
     }
 
     componentDidMount() {
-      const canvas:any = this.canasRef.current;// this.refs.canvas;
+      window.addEventListener("resize", this.onResize);
+      const canvas:any = this.canvasRef.current;
       const w: number = canvas.clientWidth;
       const h: number = canvas.clientHeight;
       this.viewBox.resize(w, h);
@@ -29,6 +34,29 @@ export default class TViewBox extends Component<IViewBoxProps, {}> {
       //ctx.drawImage(this.viewBox.Context.canvas, 0, 0);
       const bitmapOne = this.viewBox.Canvas.transferToImageBitmap();
       ctx.transferFromImageBitmap(bitmapOne);
+    }
+
+    private onResize(){
+      console.log('resize');
+      //this.canvasRef.current = React.createRef();
+      //const canvas:any = this.canvasRef.current
+      //if (canvas) {
+      //  console.log(canvas)
+      //}
+      //const w: number = canvas.clientWidth;
+      //const h: number = canvas.clientHeight;
+    }
+
+    componentWillMount() {
+      //this.onResize();
+  }
+     
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.onResize);
+    }
+
+    private setCanvasRef(element: any) {
+      this.canvasRef = element;
     }
 
     render() {
@@ -40,7 +68,7 @@ export default class TViewBox extends Component<IViewBoxProps, {}> {
             height: `${height}${mu}`
           }}>
             <h3>TViewBox</h3>
-            <canvas ref={this.canasRef} className='Trands canvas' style={{background: '#f4f4f4'}}></canvas>
+            <canvas ref={this.canvasRef} className='Trands canvas' style={{background: '#f4f4f4'}}></canvas>
         </div>
       )
     }
