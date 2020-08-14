0f8a4eb8f010bad5c5aaa05302a3e83fe8a852b6
Gerasim Gerasimov <dialix@yandex.ru>
2020-08-14 17:23:23 +0700

    вывел график, надо рихтовать его дальше

--- src/lib/trands/view/TViewUInt16.ts
+++ src/lib/trands/view/TViewUInt16.ts
@@ -16,31 +16,64 @@ export class TViewUInt16 extends TViewTrand {
   }
 
   public draw(props: IViewTrandDrawMethodProps): void {
-    const max = this.model.getMaxValue(props.fromIdx, this.Sizes.count);
-    const s = `${this.TrandProp.tag}: ${this.model.EndIndex} max: ${max}`
+    //const max = this.model.getMaxValue(props.fromIdx, this.Sizes.count);
+    props.ctx.clearRect(0, 0, this.Sizes.width, this.Sizes.height);
+    const s = `${this.TrandProp.tag}: ${this.model.EndIndex}`;// max: ${max}
     props.ctx.strokeStyle = this.TrandProp.color;
     props.ctx.strokeText(s, 150, 20);
     //TODO написать функцию отрисовки графика
+    //отрисовка Оси
     const AxisProps: IAxisProps = {
       ctx: props.ctx,
       AxisY: this.Scales.Axis,
       width: this.Sizes.width,
       color: this.TrandProp.color
     }
-    this.drawAxis(AxisProps)
+    this.drawAxis(AxisProps);
+    //вычислить вертикальную шкалу HScale
+    this.Scales.HScale = this.getHScale(props.fromIdx);
+    //собственна график
+    this.drawLineGraph(props.ctx, props.fromIdx);
   }
   
   private drawAxis(props: IAxisProps) {
-    //props.ctx.
     props.ctx.moveTo(0, props.AxisY);
     props.ctx.lineTo(props.width, props.AxisY);
     props.ctx.stroke();
+  }
+
+  private drawLineGraph(ctx: any, fromIdx: number) {
+    let count: number = this.Sizes.count;
+    let idx: number = fromIdx;
+    let y: number = this.getScaledY(idx++)
+    let x: number = 0;
+    ctx.moveTo(x,y)
+    while (count-- != 0) {
+      y = this.getScaledY(idx++);
+      x += this.Scales.WScale;
+      ctx.lineTo(x,y);
+    }
+    ctx.stroke();
+  }
+
+  private getScaledY(index: number): number {
+    const value: number = this.model.getValueByIndex(index);
+    const scaledValue: number = value * this.Scales.HScale;
+    const res = this.Scales.Axis - scaledValue;
+    return res | 0
+  }
+
+  private getHScale(fromIdx: number): number {
+    const max: number = this.model.getMaxValue(fromIdx, this.Sizes.count);
+    const HScale: number = ((this.Sizes.height - this.Scales.Axis) / max) || 1;
+    return HScale;
   }
 
   public resize(sizes: IViewTrandSizeProp): void {
     const {width, height, count} = {... sizes};
     //при ресайзе меняются:
     //0) ширина-высота области отображения
+    this.Sizes.count = count;
     this.Sizes.height = height;
     this.Sizes.width = width;
     //1) Положение оси (задано в % от высоты)
