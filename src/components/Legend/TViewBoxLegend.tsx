8aa15f8a177f5d55408aacb7b4f0b3d22f0830c1
Gerasim Gerasimov <dialix@yandex.ru>
2020-09-07 18:25:59 +0700

    c table получается интересней, ширина будет зависеть от контента и + всё будет выровнено

--- src/components/Legend/TViewBoxLegend.tsx
+++ src/components/Legend/TViewBoxLegend.tsx
@@ -16,6 +16,28 @@ export default class TViewBoxLegend extends Component<IViewBoxLegendProps, IView
     render() {
       return (
         <div className='legend'>
+          <table>
+            <tr>
+              <td><ColorMark color={'green'}/></td>
+              <td>U2/Iexc</td>
+              <td>100.5</td>
+              <td>А</td>
+            </tr>
+            <tr>
+              <td><ColorMark color={'gray'}/></td>
+              <td>U2/Ustat</td>
+              <td>11340</td>
+              <td>B</td>
+            </tr>
+          </table>
+
+        </div>
+      )
+    }
+}
+
+//TODO сделать легенду к графику
+/*
               <div className='legend__item flex_row'>
                 <ColorMark color={'green'}/>
                 <span >U2/Iexc:</span>
@@ -26,9 +48,4 @@ export default class TViewBoxLegend extends Component<IViewBoxLegendProps, IView
                 <span >U2/Ustat:</span>
                 <span >6300 А</span>
               </div>
-        </div>
-      )
-    }
-}
-
-//TODO сделать легенду к графику
+*/
