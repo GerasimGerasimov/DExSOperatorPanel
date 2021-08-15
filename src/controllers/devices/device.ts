f02ebca734cbe1ce585711d81f402f669301d340
Gerasim Gerasimov <dialix@yandex.ru>
2021-08-15 21:06:20 +0700

    в режиме дебага на ПК поставляет URL для данных удалённого хоста. Реальные данные в Trands (был генератор)

--- src/controllers/devices/device.ts
+++ src/controllers/devices/device.ts
@@ -1,4 +1,8 @@
-const HOST: string = `${window.location.protocol}//${window.location.hostname}`;
+const isDebug = true;
+const DEBUG_HOST = ' http://192.168.1.100';
+const HOST: string = (isDebug)
+                    ? DEBUG_HOST
+                    : `${window.location.protocol}//${window.location.hostname}`;
 const urlDevicesValuesGet: string = `${HOST}:5004/v1/devices/`;
 const urlDevicesValuesWrite: string = `${HOST}:5004/v1/values/`;
 const urlDevicesInfo: string = `${HOST}:5004/v1/info/`;
